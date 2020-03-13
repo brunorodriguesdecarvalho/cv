e37bc6a92bef92876647099b0e4c4262e7a59530
brunorodriguesdecarvalho <rodrigues_b@hotmail.com>
2020-03-13 10:51:07 -0300

    Alterações de layout + inclusão de JS para esconder e mostrar itens

---
+++ src/div.js
@@ -0,0 +1,83 @@
+function mostraExPro() {
+    var job1 = document.getElementById("ExPro1");
+    var job2 = document.getElementById("ExPro2");
+    var job3 = document.getElementById("ExPro3");
+    var job4 = document.getElementById("ExPro4");
+    var job5 = document.getElementById("ExPro5");
+    var job6 = document.getElementById("ExPro6");
+    var job7 = document.getElementById("ExPro7");
+    var job8 = document.getElementById("ExPro8");
+    var job9 = document.getElementById("ExPro9");
+    var job10 = document.getElementById("ExPro10");
+    if (job1.style.display != "none") {
+            job1.style.display = "none";
+            job2.style.display = "none";
+            job3.style.display = "none";
+            job4.style.display = "none";
+            job5.style.display = "none";
+            job6.style.display = "none";
+            job7.style.display = "none";
+            job8.style.display = "none";
+            job9.style.display = "none";
+            job10.style.display = "none";
+        }  else {
+            job1.style.display = "block";
+            job2.style.display = "block";
+            job3.style.display = "block";
+            job4.style.display = "block";
+            job5.style.display = "block";
+            job6.style.display = "block";
+            job7.style.display = "block";
+            job8.style.display = "block";
+            job9.style.display = "block";
+            job10.style.display = "block";
+        }     
+}
+
+function mostraEsc() {
+    var Esc1 = document.getElementById("Esc1");
+    var Esc2 = document.getElementById("Esc2");
+    var Esc3 = document.getElementById("Esc3");
+    var Esc4 = document.getElementById("Esc4");
+    var Esc5 = document.getElementById("Esc5");
+    var Esc6 = document.getElementById("Esc6");
+    if (Esc1.style.display != "none") {
+            Esc1.style.display = "none";
+            Esc2.style.display = "none";
+            Esc3.style.display = "none";
+            Esc4.style.display = "none";
+            Esc5.style.display = "none";
+            Esc6.style.display = "none";
+        }  else {
+            Esc1.style.display = "block";
+            Esc2.style.display = "block";
+            Esc3.style.display = "block";
+            Esc4.style.display = "block";
+            Esc5.style.display = "block";
+            Esc6.style.display = "block";
+        }     
+}
+
+function mostraCurso() {
+    var Curso1 = document.getElementById("Curso1");
+    var Curso2 = document.getElementById("Curso2");
+    var Curso3 = document.getElementById("Curso3");
+    var Curso4 = document.getElementById("Curso4");
+    var Curso5 = document.getElementById("Curso5");
+    var Curso6 = document.getElementById("Curso6");
+    if (Curso1.style.display != "none") {
+            Curso1.style.display = "none";
+            Curso2.style.display = "none";
+            Curso3.style.display = "none";
+            Curso4.style.display = "none";
+            Curso5.style.display = "none";
+            Curso6.style.display = "none";
+        }  else {
+            Curso1.style.display = "block";
+            Curso2.style.display = "block";
+            Curso3.style.display = "block";
+            Curso4.style.display = "block";
+            Curso5.style.display = "block";
+            Curso6.style.display = "block";
+        }     
+}
