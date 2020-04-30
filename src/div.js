d065e01c31f41bb31f46505ac5c62a1c690389c3
brunorodriguesdecarvalho <rodrigues_b@hotmail.com>
2020-04-30 12:20:08 -0300

    atualização de conteúdo e layout

--- src/div.js
+++ src/div.js
@@ -1,14 +1,19 @@
 function mostraExPro() {
-    var job1 = document.getElementById("ExPro1");
-    var job2 = document.getElementById("ExPro2");
-    var job3 = document.getElementById("ExPro3");
-    var job4 = document.getElementById("ExPro4");
-    var job5 = document.getElementById("ExPro5");
-    var job6 = document.getElementById("ExPro6");
-    var job7 = document.getElementById("ExPro7");
-    var job8 = document.getElementById("ExPro8");
-    var job9 = document.getElementById("ExPro9");
-    var job10 = document.getElementById("ExPro10");
+    const job1 = document.getElementById("ExPro1");
+    const job2 = document.getElementById("ExPro2");
+    const job3 = document.getElementById("ExPro3");
+    const job4 = document.getElementById("ExPro4");
+    const job5 = document.getElementById("ExPro5");
+    const job6 = document.getElementById("ExPro6");
+    const job7 = document.getElementById("ExPro7");
+    const job8 = document.getElementById("ExPro8");
+    const job9 = document.getElementById("ExPro9");
+    const job10 = document.getElementById("ExPro10");
+    const job11 = document.getElementById("ExPro11");
+    const job12 = document.getElementById("ExPro12");
+    const job13 = document.getElementById("ExPro13");
+    const job14 = document.getElementById("ExPro14");
+    const job15 = document.getElementById("ExPro15");
     if (job1.style.display != "none") {
             job1.style.display = "none";
             job2.style.display = "none";
@@ -20,6 +25,11 @@ function mostraExPro() {
             job8.style.display = "none";
             job9.style.display = "none";
             job10.style.display = "none";
+            job11.style.display = "none";
+            job12.style.display = "none";
+            job13.style.display = "none";
+            job14.style.display = "none";
+            job15.style.display = "none";
         }  else {
             job1.style.display = "block";
             job2.style.display = "block";
@@ -31,16 +41,21 @@ function mostraExPro() {
             job8.style.display = "block";
             job9.style.display = "block";
             job10.style.display = "block";
+            job11.style.display = "block";
+            job12.style.display = "block";
+            job13.style.display = "block";
+            job14.style.display = "block";
+            job15.style.display = "block";
         }     
 }
 
 function mostraEsc() {
-    var Esc1 = document.getElementById("Esc1");
-    var Esc2 = document.getElementById("Esc2");
-    var Esc3 = document.getElementById("Esc3");
-    var Esc4 = document.getElementById("Esc4");
-    var Esc5 = document.getElementById("Esc5");
-    var Esc6 = document.getElementById("Esc6");
+    const Esc1 = document.getElementById("Esc1");
+    const Esc2 = document.getElementById("Esc2");
+    const Esc3 = document.getElementById("Esc3");
+    const Esc4 = document.getElementById("Esc4");
+    const Esc5 = document.getElementById("Esc5");
+    const Esc6 = document.getElementById("Esc6");
     if (Esc1.style.display != "none") {
             Esc1.style.display = "none";
             Esc2.style.display = "none";
@@ -59,12 +74,12 @@ function mostraEsc() {
 }
 
 function mostraCurso() {
-    var Curso1 = document.getElementById("Curso1");
-    var Curso2 = document.getElementById("Curso2");
-    var Curso3 = document.getElementById("Curso3");
-    var Curso4 = document.getElementById("Curso4");
-    var Curso5 = document.getElementById("Curso5");
-    var Curso6 = document.getElementById("Curso6");
+    const Curso1 = document.getElementById("Curso1");
+    const Curso2 = document.getElementById("Curso2");
+    const Curso3 = document.getElementById("Curso3");
+    const Curso4 = document.getElementById("Curso4");
+    const Curso5 = document.getElementById("Curso5");
+    const Curso6 = document.getElementById("Curso6");
     if (Curso1.style.display != "none") {
             Curso1.style.display = "none";
             Curso2.style.display = "none";
@@ -80,4 +95,26 @@ function mostraCurso() {
             Curso5.style.display = "block";
             Curso6.style.display = "block";
         }     
+}
+
+function mostraEu() {
+    const eu = document.getElementById("eu");
+    if (eu.style.display != "none") {eu.style.display = "none";}  
+    else {eu.style.display = "block";}     
+}
+
+function mostraSkill() {
+    const skill = document.getElementById("skill");
+    if (skill.style.display != "none") {skill.style.display = "none";}  
+    else {skill.style.display = "block";}     
+}
+
+function mostraContato() {
+    const Contato = document.getElementById("Contato");
+    if (Contato.style.display != "none") {
+        Contato.style.display = "none";
+    }  
+    else {
+        Contato.style.display = "block";
+    }     
 }
